95ba462c60215de598509366366628600ed6898a
mbv <mbv@WIN-KAFE6A6HJRV>
2017-05-06 15:28:20 +0300

    Add project files.

---
+++ XamarinLab/XamarinLab/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace XamarinLab.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
